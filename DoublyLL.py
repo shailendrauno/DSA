07e93f37d47f9cac7c1400c9192a106fa53407e3
Shailendra Kumar <shailendrauno89@gmail.com>
2026-02-08 11:25:16 +0530

    Complete

--- DoublyLL.py
+++ DoublyLL.py
@@ -4,104 +4,96 @@ class Node:
         self.prev = None
         self.next = None
 
-class doublyLL :
+class doublyLL:
     def __init__(self):
         self.head = None
 
-    def insertAtEnd (self, data):
+    def insertAtEnd(self, data):
         new_node = Node(data)
-
-        if (self.head == None):
+        if self.head is None:
             self.head = new_node
             return
         temp = self.head
-        while  temp.next != None:
+        while temp.next:
             temp = temp.next
         temp.next = new_node
         new_node.prev = temp
 
-    def insertAtBig (self, data):
+    def insertAtBig(self, data):
         new_node = Node(data)
-        if(self.head == None):
+        if self.head is None:
             self.head = new_node
             return
         new_node.next = self.head
-        self.head = new_node
         self.head.prev = new_node
+        self.head = new_node
 
-
-    def insertAtSP (self, data, index):
-        if(index == 0):
-            self.insertAtBig()
+    def insertAtSP(self, data, index):
+        if index == 0:
+            self.insertAtBig(data)
             return
         new_node = Node(data)
         temp = self.head
-        for i in range ( index - 1):
+        for i in range(index - 1):
             if temp is None:
-                print("The index is out of bound")
+                print("Index out of range")
                 return
             temp = temp.next
-            
+
         new_node.next = temp.next
         new_node.prev = temp
+
+        if temp.next is not None:
+            temp.next.prev = new_node
+
         temp.next = new_node
 
     def deleteAtLast(self):
-        if (self.head is None):
+        if self.head is None:
             print("List is empty")
             return
-        if (self.head.next is None):
+        if self.head.next is None:
             self.head = None
             return
         temp = self.head
-        while (temp.next != None):
+        while temp.next:
             temp = temp.next
         temp.prev.next = None
 
     def deleteAtBig(self):
-        if(self.head is None):
+        if self.head is None:
             print("List is empty")
             return
-        if(self.head.next is None):
+        if self.head.next is None:
             self.head = None
             return
         self.head = self.head.next
         self.head.prev = None
-        
+
     def deleteAtSP(self, idx):
-        if(idx == 0):
+        if idx == 0:
             self.deleteAtBig()
             return
         temp = self.head
         for i in range(idx):
             if temp is None:
-                print("Position out of range")
+                print("Index out of range")
                 return
             temp = temp.next
+
+        if temp is None:
+            print("Index out of range")
+            return
 
         if temp.next is not None:
             temp.next.prev = temp.prev
 
         if temp.prev is not None:
-         temp.prev.next = temp.next
-
+            temp.prev.next = temp.next
 
     def display(self):
         temp = self.head
-        while temp != None:
-            print(temp.data, end= " <--> ")
+        while temp:
+            print(temp.data, end=" <-> ")
             temp = temp.next
         print("None")
-
-
-dl = doublyLL()
-dl.insertAtEnd(10)
-dl.insertAtEnd(20)
-dl.insertAtEnd(30)
-dl.insertAtBig(5)
-dl.insertAtBig(2)
-dl.insertAtSP(50,5)
-dl.deleteAtLast()
-dl.deleteAtBig()
-dl.deleteAtSP(1)
-dl.display()
