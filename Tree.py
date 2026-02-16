afe318d6bcd08c51226e8ff07b9f1f5ea5b84d9e
Shailendra Kumar <shailendrauno89@gmail.com>
2026-02-16 09:40:30 +0530

    Tree is complete

--- Tree.py
+++ Tree.py
@@ -47,24 +47,3 @@ preOrder(root)
 inOrder(root)
 postOrder(root)
 
-
-
-
-class Node :
-    def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
-
-def preOrder (root):
-    if (root != None):
-        print(root.data, end= " ")
-        preOrder(root.left)
-        preOrder(root.right)
-
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-preOrder(root)
