3907ce94e09aa30136121d8047184c7e364917cf
Shailendra Kumar <shailendrauno89@gmail.com>
2026-02-23 10:07:53 +0530

    this is the bubble sort is complete

--- bobblesort.py
+++ bobblesort.py
@@ -1,4 +1,17 @@
-def bubblesort(a):
+
+def bubblesortDes (a):
+    n = len(a)
+
+    for i in range(n):
+        for j in range (0, n-1-i):
+            if(a[j+1] > a[j]):
+                a[j+1], a[j] = a[j], a[j+1]
+a = [1,2,3,4,5,6,7,8,9]
+bubblesortDes(a)
+print(a)
+
+
+def bubblesortAss(a):
     n = len(a)
 
     for i in range(n):
@@ -7,14 +20,5 @@ def bubblesort(a):
                 a[j], a[j+1] = a[j+1], a[j]
 
 a = [12,11,8,5,4,32,1]
-bubblesort(a)
+bubblesortAss(a)
 print(a)
-
-
-def bubblesort (a):
-    n = len(a)
-
-    for i in range(n):
-        for j in range (0, j-1-i):
-            if(a[j+1] > a[j]):
-                a[j+1], a[j] = a[j], a[j+1]
