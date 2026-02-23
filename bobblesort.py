5335d1f95e9a2a40a8bd0646ec11a36f3dc455b4
Shailendra Kumar <shailendrauno89@gmail.com>
2026-02-23 10:04:41 +0530

    hello sir

--- bobblesort.py
+++ bobblesort.py
@@ -9,3 +9,12 @@ def bubblesort(a):
 a = [12,11,8,5,4,32,1]
 bubblesort(a)
 print(a)
+
+
+def bubblesort (a):
+    n = len(a)
+
+    for i in range(n):
+        for j in range (0, j-1-i):
+            if(a[j+1] > a[j]):
+                a[j+1], a[j] = a[j], a[j+1]
