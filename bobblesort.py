390fd8a5cd692a170e2b4d301b3419e7fd8fb18a
Shailendra Kumar <shailendrauno89@gmail.com>
2026-03-08 10:26:39 +0530

    this is ten firtst commkt

--- bobblesort.py
+++ bobblesort.py
@@ -1,17 +1,4 @@
-
-# def bubblesortDes (a):
-#     n = len(a)
-
-#     for i in range(n):
-#         for j in range (0, n-1-i):
-#             if(a[j+1] > a[j]):
-#                 a[j+1], a[j] = a[j], a[j+1]
-# a = [1,2,3,4,5,6,7,8,9]
-# bubblesortDes(a)
-# print(a)
-
-
-def bubblesortAss(a):
+def bubbleShort(a):
     n = len(a)
 
     for i in range(n):
@@ -19,6 +6,6 @@ def bubblesortAss(a):
             if(a[j] > a[j+1]):
                 a[j], a[j+1] = a[j+1], a[j]
 
-a = [12,11,8,5,4,32,1]
-bubblesortAss(a)
+a = [23,34,43,2,13,1,76]
+bubbleShort(a)
 print(a)
