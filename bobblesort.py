53870a17ddb3ec5623c5bdc8d7bc49149bbb2622
Shailendra Kumar <shailendrauno89@gmail.com>
2026-03-08 09:15:49 +0530

    this is trial purpus

--- bobblesort.py
+++ bobblesort.py
@@ -1,14 +1,14 @@
 
-def bubblesortDes (a):
-    n = len(a)
+# def bubblesortDes (a):
+#     n = len(a)
 
-    for i in range(n):
-        for j in range (0, n-1-i):
-            if(a[j+1] > a[j]):
-                a[j+1], a[j] = a[j], a[j+1]
-a = [1,2,3,4,5,6,7,8,9]
-bubblesortDes(a)
-print(a)
+#     for i in range(n):
+#         for j in range (0, n-1-i):
+#             if(a[j+1] > a[j]):
+#                 a[j+1], a[j] = a[j], a[j+1]
+# a = [1,2,3,4,5,6,7,8,9]
+# bubblesortDes(a)
+# print(a)
 
 
 def bubblesortAss(a):
